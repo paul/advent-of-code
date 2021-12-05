2106c9e11313407ee61f5811fddaf6fec15f9b3b
Paul Sadauskas <psadauskas@gmail.com>
2021-12-05 13:39:09 -0700

    🏌

--- 2021/day1/main.cr
+++ 2021/day1/main.cr
@@ -1,12 +1,10 @@
-lines = File.read_lines("input.txt") 
 last = nil
 num_increased = 0
-lines.each do |line|
+
+File.read_lines("input.txt").each do |line|
   num = line.to_i
 
-  if last && num > last
-    num_increased += 1
-  end
+  num_increased += 1 if last && num > last
 
   last = num
 end
